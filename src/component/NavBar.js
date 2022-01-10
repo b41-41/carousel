3f1cc6ce43cdea98015ecded6b2bce9be06d7329
b41-41 <41@b41.kr>
2022-01-10 20:20:03 +0900

    FEAT 네비게이션 마크업
    아이콘 파일 추가 후 '원티드' 사이트에 있는 네비게이션 메뉴 마크업.

---
+++ src/component/NavBar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import menuImg from '../images/icon-menu.png';
+import logoImg from '../images/logo.png';
+import '../css/navBar.css';
+
+const NavBar = () => {
+    return (
+        <>
+            <div className="MainBar_MainBar">
+                <div className="MainBar_MainBar_nav">
+                    <div className="MainBar_MainBar_nav_top">
+                        <button type="button"><img src={menuImg} alt="menu" width="17px" height="14px" /></button>
+                        <a href="/"><img src={logoImg} alt="logo" width="75px" height="17px" /></a>
+                    </div>
+                    <ul className="Menu_className">
+                        <li>채용</li>
+                        <li>이벤트</li>
+                        <li>직군별 연봉</li>
+                        <li>이력서</li>
+                        <li>커뮤니티<span>New</span></li>
+                        <li>프리랜서</li>
+                        <li>AI 합격예측<span>New</span></li>
+                    </ul>
+                    <aside className="Aside_className">
+                        <ul>
+                            <li>
+                                <button clasName="searchButton" type="button">🔍
+                                </button>
+                            </li>
+                            <li>
+                                <button className="signUpButton" type="button">회원가입/로그인</button>
+                            </li>
+                            <li>
+                                <a className="dashboardButton" href="/dashboard">기업 서비스</a>
+                            </li>
+                        </ul>
+                    </aside>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default NavBar;
---
+++ src/pages/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from '../component/NavBar';
+
+const Main = () => {
+
+    return (
+
+        <NavBar />
+
+    );
+};
+
+export default Main;
