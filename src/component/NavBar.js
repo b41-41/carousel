5b4caf08decd017f63186db7e91ab83ef2069d72
b41-41 <41@b41.kr>
2022-01-11 01:54:24 +0900

    STYLE 네비게이션 반응형 스타일 적용
    미디어쿼리를 사용하여 768px, 991px, 1178px, 그 이상 으로 나눠서 페이지 크기 별로
    네비게이션의 형태가 반응형으로 바뀌도록 구현.

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,40 +1,50 @@
 import React from 'react';
 import menuImg from '../images/icon-menu.png';
 import logoImg from '../images/logo.png';
+import searchImg from '../images/search.png';
+import dotImg from '../images/dots.png';
 import '../css/navBar.css';
 
 const NavBar = () => {
     return (
         <>
-            <div className="MainBar_MainBar">
-                <div className="MainBar_MainBar_nav">
-                    <div className="MainBar_MainBar_nav_top">
-                        <button className="MainBar_hamberger" type="button"><img src={menuImg} alt="menu" width="17px" height="14px" /></button>
-                        <a href="/"><img src={logoImg} alt="logo" width="75px" height="17px" /></a>
-                    </div>
-                    <ul className="Menu_className">
-                        <li><a href="#">채용</a></li>
-                        <li><a href="#">이벤트</a></li>
-                        <li><a href="#">직군별 연봉</a></li>
-                        <li><a href="#">이력서</a></li>
-                        <li><a href="#">커뮤니티<em>New</em></a></li>
-                        <li><a href="#">프리랜서</a></li>
-                        <li><a href="#">AI 합격예측<em>Beta</em></a></li>
-                    </ul>
-                    <aside className="Aside_className">
-                        <ul>
-                            <li>
-                                <button clasName="searchButton" type="button">🔍
-                                </button>
-                            </li>
-                            <li>
-                                <button className="signUpButton" type="button">회원가입/로그인</button>
-                            </li>
-                            <li>
-                                <a className="dashboardButton" href="/dashboard">기업 서비스</a>
-                            </li>
+            <div className="NavBar_ClassName">
+                <div className="MainBar_MainBar">
+                    <div className="MainBar_MainBar_nav">
+                        <div className="MainBar_MainBar_nav_top">
+                            <div className="Mainbar_Mainbar_nav_top_logo">
+                                <button className="MainBar_hamberger" type="button"><img src={menuImg} alt="menu" width="17px" height="14px" /></button>
+                                <a href="/"><img src={logoImg} alt="logo" width="75px" height="17px" /></a>
+                            </div>
+                            <button class="xsSignUpButton" type="button">회원가입하기</button>
+                        </div>
+                        <ul className="Menu_className">
+                            <li className='Menu_className_home'><a href="/">홈</a></li>
+                            <li><a href="/jobsfeed">채용</a></li>
+                            <li><a href="/events">이벤트</a></li>
+                            <li className="Menu_className_option"><a href="/salary">직군별 연봉</a></li>
+                            <li className="Menu_className_option"><a href="/cv/intro">이력서</a></li>
+                            <li className="Menu_className_option"><a href="/community">커뮤니티<em>New</em></a></li>
+                            <li className="Menu_className_option"><a href="#">프리랜서</a></li>
+                            <li className="Menu_className_option"><a href="aiscore/resume">AI 합격예측<em>Beta</em></a></li>
                         </ul>
-                    </aside>
+                        <aside className="Aside_className">
+                            <ul>
+                                <li>
+                                    <button className="searchButton" type="button"><img src={searchImg} alt="search" height="15px" width="15px" />
+                                    </button>
+                                    <button className="menuButton" type="button"><img src={dotImg} alt="menu" width="18px" height="18px" /></button>
+                                </li>
+                                <li>
+                                    <button className="signUpButton" type="button">회원가입/로그인</button>
+                                </li>
+                                <li className="mdMoreVisible">
+                                    <div className="before_line"></div>
+                                    <a className="dashboardButton" href="/dashboard">기업 서비스</a>
+                                </li>
+                            </ul>
+                        </aside>
+                    </div>
                 </div>
             </div>
         </>
